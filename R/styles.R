800c59b3f05d4ecf13471a18b2342e1ea6004764
Susanna Marquez <susanna.marquez@yale.edu>
2023-04-18 18:14:30 +0200

    use arial

--- R/styles.R
+++ R/styles.R
@@ -1,10 +1,10 @@
 #' @export
 theme_enchantr <- function() {
-    ggplot2::theme_bw() +
+    ggplot2::theme_bw(base_family = "ArialMT") +
         theme(strip.background=element_blank(),
               plot.background=element_blank(),
               panel.grid.major=element_blank(), 
-              panel.grid.minor=element_blank()
+              panel.grid.minor=element_blank(),
+              text = element_text(family="ArialMT")
               )
 }
-
