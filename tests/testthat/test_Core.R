b45ac3c56035d5cada7350f20ff43b5034838aae
Gisela Gabernet <gisela.gabernet@gmail.com>
2023-10-13 16:57:16 -0400

    add test input validation define clones

--- tests/testthat/test_Core.R
+++ tests/testthat/test_Core.R
@@ -1,10 +1,12 @@
-# test_that("input_validation report runs", {
-#     tmp_dir <- file.path(tempdir(),"test_input_validation","")
-#     enchantr_report('validate_input', report_params=list('outdir'=tmp_dir))
-# })
+test_that("input_validation report runs", {
+    tmp_dir <- file.path(tempdir(),"test_input_validation","")
+    enchantr_report('validate_input', report_params=list('outdir'=tmp_dir))
+    expect_true(file.exists(file.path(tmp_dir,"enchantr","index.html")))
+})
 
 
-# test_that("file_size report runs", {
-#     tmp_dir <- file.path(tempdir(),"file_size","")
-#     enchantr_report('file_size', report_params=list('outdir'=tmp_dir))
-# })
+test_that("define_clones report runs", {
+    tmp_dir <- file.path(tempdir(),"test_define_clones","")
+    enchantr_report('define_clones', report_params=list('outdir'=tmp_dir))
+    expect_true(file.exists(file.path(tmp_dir,"enchantr","index.html")))
+})
