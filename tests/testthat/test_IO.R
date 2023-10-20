ed17b9a811e06cbbe6c14ee371946ded0b5b31c9
Susanna Marquez <susanna.marquez@yale.edu>
2023-10-20 13:43:24 +0200

    typo

--- tests/testthat/test_IO.R
+++ tests/testthat/test_IO.R
@@ -3,7 +3,7 @@ test_that("read input url", {
     skip_on_cran()
     
     url <- "https://zenodo.org/records/8179846/files/BCR_data_sample1.tsv"
-    # TOD: expect no errors or warning once file in Zenodo updated
+    # TODO: expect no errors or warning once file in Zenodo updated
     # with rev_comp = FALSE
     expect_warning(readInput(url), "rev_comp is not logical for row\\(s\\):")
     #expect_no_error(readInput(url))
