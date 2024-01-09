83f65bdc76573ef50f2418bdc6b5c128cd7c6b9e
Susanna Marquez <susanna.marquez@yale.edu>
2024-01-09 17:48:08 +0100

    added local copy of fuse
    This is to avoid the error:
       Could not fetch https://cdn.jsdelivr.net/npm/fuse.js@6.4.6/dist/fuse.min.js
    that happens rendering the reports without access to the internet

--- R/format.R
+++ R/format.R
@@ -47,12 +47,15 @@ immcantation <- function(...) {
         ),
         "highlight"="pygments"
     )
-    if (!is.null(user_args)){
+    if (!is.null(user_args)) {
       immcantation_config <- modifyList(
         immcantation_config, 
         user_args, 
         keep.null = T) 
     }
+    
+    # overwrite bookdown:::gitbook_dependency
+    assignInNamespace("gitbook_dependency",gitbook_dependency,ns="bookdown")
     
     # call the base html_document function
     b <- bookdown::gitbook(
@@ -67,4 +70,43 @@ immcantation <- function(...) {
                       code_folding="hide", self_contained=TRUE, split_by = "none" 
                       )
     invisible(b)
+}
+
+
+# This function overwrites bookdown:::gitbook_dependency
+# to use a local copy of fuse.js.
+# The original function has a hardcoded url to download fuse. fuse is the search
+# engine for the reports. This is a proble for users in controlled environments 
+# without internet access because the can't render the reports. The error is:
+# "Could not fetch https://cdn.jsdelivr.net/npm/fuse.js@6.4.6/dist/fuse.min.js"
+gitbook_dependency = function(table_css, config = list()) {
+    assets = bookdown:::bookdown_file('resources', 'gitbook')
+    owd = setwd(assets); on.exit(setwd(owd), add = TRUE)
+    app = if (file.exists('js/app.min.js')) 'app.min.js' else 'app.js'
+    # TODO: download and a local copy of fuse.js?
+    fuse = htmltools::htmlDependency(
+        'fuse', '6.4.6', c('file' = system.file("js","fuse.js", package = "enchantr", lib.loc = NULL,
+                                                mustWork = FALSE)),
+        script = 'fuse.js@6.4.6'
+    )
+    if (is.logical(config$search)) {
+        lunr = FALSE
+        if (!config$search) fuse = NULL
+    } else {
+        # use fuse as the search engine by default
+        lunr = identical(config$search$engine, 'lunr')
+    }
+    list(bookdown:::jquery_dependency(), fuse, htmltools::htmlDependency(
+        'gitbook', '2.6.7', src = assets,
+        stylesheet = file.path('css', c(
+            'style.css', if (table_css) 'plugin-table.css', 'plugin-bookdown.css',
+            'plugin-highlight.css', 'plugin-search.css', 'plugin-fontsettings.css',
+            'plugin-clipboard.css'
+        )),
+        script = file.path('js', c(
+            app, if (lunr) 'lunr.js', 'clipboard.min.js', 'plugin-search.js', 'plugin-sharing.js',
+            'plugin-fontsettings.js', 'plugin-bookdown.js', 'jquery.highlight.js',
+            'plugin-clipboard.js'
+        ))
+    ))
 }
