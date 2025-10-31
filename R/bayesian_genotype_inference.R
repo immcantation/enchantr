26b2e849b7f095fddb2bfcd7e13eaf972c1f40c3
ayeletperes <aperes7@gmail.com>
2025-10-31 13:56:39 -0400

    update genotype report

---
+++ R/bayesian_genotype_inference.R
@@ -0,0 +1,18 @@
+#' Create an Immcantation ...
+#'
+#' From RStudio, use the New Project wizard: File > New Project >
+#' New Directory > then select  Immcantation ...
+#' to create the skeleton of an Immcantation ... project
+#' @param  path path to the directory where the project will be created
+tigger_bayesian_genotype_project <- function(path,...) {
+    skeleton_dir <- file.path(system.file(package = "enchantr"), "rstudio",
+                              "templates", "project",
+                              "tigger_bayesian_genotype_project_files")
+    project_dir <- path
+    if (!dir.exists(project_dir)) {
+        message("Creating project_dir ", project_dir)
+        dir.create(project_dir, recursive = TRUE, showWarnings = FALSE)
+    }
+    project_files <- list.files(skeleton_dir, full.names = TRUE)
+    file.copy(project_files, project_dir, recursive = TRUE)
+}
