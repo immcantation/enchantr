55127d73be20919f13a6ddf4ddb52b5c24bc5ede
ayelet peres <aperes7@gmail.com>
2025-11-19 13:48:06 -0500

    updated all reports, separated re-assignment from novel and genotype reports.

--- tests/testthat/test_genotype_inference.R
+++ tests/testthat/test_genotype_inference.R
@@ -1,7 +1,7 @@
 imgt_url <- "https://raw.githubusercontent.com/nf-core/test-datasets/airrflow/database-cache/imgtdb_base.zip"
 
-#TODO: find a testing set
-test_that("novel inference 1:1", {
+# TODO: find a testing set
+test_that("genotype inference 1:1", {
   # Input in one file, output in 1 file.
   # All sequences have sampletype = FNA
   skip_on_cran()
@@ -9,15 +9,16 @@ test_that("novel inference 1:1", {
   tmp_dir <- file.path(tempdir(), "genotype_inference_1_1")
   enchantr_report("tigger_bayesian_genotype",
     report_params = list(
-                         "input" = input,
-                         "imgt_db" = imgt_url,
-                         "species" = "human",
-                         "outdir" = tmp_dir,
-                         "call_column" = "v_call",
-                         "reassign" = TRUE,
-                         "log" = "test_allele_inference_command_log")
+      "input" = input,
+      "imgt_db" = imgt_url,
+      "species" = "human",
+      "outdir" = tmp_dir,
+      "log" = "test_allele_inference_command_log"
+    )
   )
-  
-  genotype <- read.delim(file.path(tmp_dir, "enchantr", "tigger_v_call_genotype_report.tsv"), sep = "\t")
-  expect_equal(nrow(genotype), 10)
+
+  report_dir <- file.path(tmp_dir, "enchantr")
+  genotypes <- list.files(file.path(report_dir, "genotypes"), full.names = TRUE)
+  genotype <- read.delim(genotypes, sep = "\t")
+  expect_equal(nrow(genotype), 41)
 })
