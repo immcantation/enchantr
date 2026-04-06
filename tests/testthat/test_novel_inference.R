1ee796a69c54a284c82dbd0a9fe54d3d094d85c3
ayelet peres <aperes7@gmail.com>
2026-04-06 12:20:02 -0400

    updated plot saving, and added novel allele pre-check

--- tests/testthat/test_novel_inference.R
+++ tests/testthat/test_novel_inference.R
@@ -5,6 +5,7 @@ test_that("novel inference 1:1", {
   # Input in one file, output in 1 file.
   # All sequences have sampletype = FNA
   skip_on_cran()
+  skip_if_not_installed("ggplot2")
   input <- normalizePath(file.path("..", "data-tests", "novel_genotype", "data_to_test_novel_alleles.tsv"))
   tmp_dir <- file.path(tempdir(), "novel_inference_1_1")
   enchantr_report("novel_allele_inference",
@@ -18,6 +19,23 @@ test_that("novel inference 1:1", {
                          "log" = "test_allele_inference_command_log")
   )
   
-  novel_alleles <- read.delim(file.path(tmp_dir, "enchantr", "tigger_novel_report.tsv"), sep = "\t")
+  report_dir <- file.path(tmp_dir, "enchantr")
+  novel_alleles <- read.delim(
+    file.path(report_dir, "tables", "tigger-novel_novel_report.tsv"),
+    sep = "\t"
+  )
   expect_equal(nrow(novel_alleles), 6)
+
+  plot_paths <- list.files(
+    file.path(report_dir, "ggplots"),
+    pattern = "^novel-allele-[0-9]+\\.RData$",
+    full.names = TRUE
+  )
+  expect_equal(length(plot_paths), nrow(novel_alleles))
+
+  load(plot_paths[[1]])
+  expect_true(exists("p_obj"))
+  expect_true(exists("p_obj_extra_objects"))
+  expect_s3_class(p_obj, "ggplot")
+  expect_no_error(ggplot2::ggplot_build(p_obj))
 })
